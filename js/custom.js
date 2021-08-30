272b69b913d9b4d6c7a88b11131929fc1fd04fd2
Flip Santos <filipe.b.santos@hotmail.com>
2021-08-30 12:26:41 -0300

    mais ajustes

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// Control Client slider items
+function clientItems(){
+  var window_w = $(window).width();
+  var oc_items = window_w < 575 ? 1 : 3;
+  document.getElementById('oc-clients-full').dataset.items = oc_items;
+}
+clientItems();
+document.addEventListener('resize', clientItems);
