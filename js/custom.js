5cf489d8f5792d4be6a38ded91a5f47598f57252
Flip Santos <filipe.b.santos@hotmail.com>
2021-08-31 10:20:29 -0300

    fix clients carousel

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
-// // Control Client slider items
-// function clientItems(){
-//   var window_w = $(window).width();
-//   var oc_items = window_w < 575 ? 1 : 3;
-//   document.getElementById('oc-clients-full').dataset.items = oc_items;
-// }
-// clientItems();
+(function(){
+    // Control Client slider items
+    var window_w = $(window).width();
+    var oc_items = window_w < 575 ? 1 : 3;
+    document.getElementById('oc-clients-full').dataset.items = oc_items;
+})();
