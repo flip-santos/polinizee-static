e2bfb47133847b80dc9eb4bb99afc7776bfc7490
Flip Santos <filipe.b.santos@hotmail.com>
2021-09-01 11:35:53 -0300

    fixing responsive client carousel

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    // Control Client slider items
-    var window_w = $(window).width();
-    var oc_items = window_w < 575 ? 1 : 3;
-    document.getElementById('oc-clients-full').dataset.items = oc_items;
-})();
